e2f3b63e1a5567a4908714fe54234b59b140e578
YI ZHANG <scaccterran@gmail.com>
2020-10-26 00:54:21 -0400

    CGM-16 get all sprites

--- CloudGameMakerServer/WebApi/Controllers/CloudStorageController.cs
+++ CloudGameMakerServer/WebApi/Controllers/CloudStorageController.cs
@@ -3,7 +3,9 @@ using Amazon.S3.Model;
 using Core.Models.GameSprites;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApi.Controllers
@@ -18,6 +20,36 @@ namespace WebApi.Controllers
         public CloudStorageController(IAmazonS3 s3)
         {
             S3 = s3;
+        }
+
+        [HttpGet]
+        [Route("sprites")]
+        public async Task<IEnumerable<SpriteFile>> GetSprites()
+        {
+            var response = await S3.ListObjectsAsync(BucketName, "sprites").ConfigureAwait(false);
+
+            var tasks = response.S3Objects.Select(async _ =>
+            {
+                var s3Object = await S3.GetObjectAsync(BucketName, _.Key).ConfigureAwait(false);
+                var bytes = new byte[(int)s3Object.ResponseStream.Length];
+
+                using (var stream = new MemoryStream())
+                {
+                    await s3Object.ResponseStream.CopyToAsync(stream).ConfigureAwait(false);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    stream.Read(bytes, 0, (int)stream.Length);
+                }
+
+                return new SpriteFile
+                {
+                    Name = s3Object.Key,
+                    Type = "",
+                    Extension = "",
+                    Base64 = Convert.ToBase64String(bytes)
+                };
+            });
+
+            return await Task.WhenAll(tasks).ConfigureAwait(false);
         }
 
         [HttpPost]
