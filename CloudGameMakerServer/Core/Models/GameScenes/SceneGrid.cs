9095f51d8adda34e1ed17c097a4c4d828c38d66a
YI ZHANG <scaccterran@gmail.com>
2020-11-22 13:27:38 -0500

    CGM-25 store reference of sprites

--- CloudGameMakerServer/Core/Models/GameScenes/SceneGrid.cs
+++ CloudGameMakerServer/Core/Models/GameScenes/SceneGrid.cs
@@ -3,6 +3,5 @@ namespace Core.Models.GameScenes
     public class SceneGrid
     {
         public string SpriteId { get; set; }
-        public string ThumbnailUrl { get; set; }
     }
 }
--- CloudGameMakerServer/Core/Models/GameScenes/SceneLayer.cs
+++ CloudGameMakerServer/Core/Models/GameScenes/SceneLayer.cs
@@ -1,3 +1,4 @@
+using Core.Models.GameSprites;
 using System.Collections.Generic;
 
 namespace Core.Models.GameScenes
@@ -7,6 +8,7 @@ namespace Core.Models.GameScenes
         public string Name { get; set; }
         public int Rows { get; set; }
         public int Columns { get; set; }
+        public Dictionary<string, SpriteFile> Sprites { get; set; } = new Dictionary<string, SpriteFile>();
         public Dictionary<string, SceneGrid> Grids { get; set; } = new Dictionary<string, SceneGrid>();
     }
 }
