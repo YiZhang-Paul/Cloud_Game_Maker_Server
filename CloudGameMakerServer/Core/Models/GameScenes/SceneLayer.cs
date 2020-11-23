edf4587acac63050f88a4ad8ab46e7dcb42ebc9e
YI ZHANG <scaccterran@gmail.com>
2020-11-23 00:14:37 -0500

    CGM-8 allow hiding layer

--- CloudGameMakerServer/Core/Models/GameScenes/SceneLayer.cs
+++ CloudGameMakerServer/Core/Models/GameScenes/SceneLayer.cs
@@ -8,6 +8,7 @@ namespace Core.Models.GameScenes
         public string Name { get; set; }
         public int Rows { get; set; }
         public int Columns { get; set; }
+        public bool IsVisible { get; set; } = true;
         public Dictionary<string, SpriteFile> Sprites { get; set; } = new Dictionary<string, SpriteFile>();
         public Dictionary<string, SceneGrid> Grids { get; set; } = new Dictionary<string, SceneGrid>();
     }
